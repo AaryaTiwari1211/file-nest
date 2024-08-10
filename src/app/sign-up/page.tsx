ea20bbf58f44548d1d99dfe1230608966c29fc25
Aarya Tiwari <aarya.tiwari@somaiya.edu>
2024-08-11 02:00:18 +0530

    Made more changes to the home link

--- src/app/sign-up/page.tsx
+++ src/app/sign-up/page.tsx
@@ -1,6 +1,17 @@
 import { SignUp } from "@clerk/nextjs";
 import Image from "next/image";
+import { useEffect } from "react";
+import { useUser } from "@clerk/nextjs";
+import { useRouter } from "next/navigation";
+
 export default function SignUpPage() {
+  const user = useUser();
+  const router = useRouter();
+  useEffect(() => {
+    if (user.user) {
+      router.push("/dashboard/files");
+    }
+  }, []);
   return (
     <div className="flex justify-center items-center my-10">
       <div className="flex max-w-[1200px] items-center justify-center mt-12 gap-10">
